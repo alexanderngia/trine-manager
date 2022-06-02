27c75235c7218f6cc5a828c632a90586aa9ab5bc
Alexander <alexanderngia@gmail.com>
2022-06-03 01:18:37 +0700

    Update customerList.tsx

--- src/components/views/customerList/customerList.tsx
+++ src/components/views/customerList/customerList.tsx
@@ -1,48 +1,48 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import styles from "./customerList.module.scss";
-import { Layout } from "../layout/layout";
+import React, { useEffect, useState } from "react";
+import { IoAdd, IoDownloadOutline, IoRemoveCircleSharp } from "react-icons/io5";
+import { useAppSelector } from "../../../hooks/useRedux";
+import customerService from "../../../services/customerService";
 import { ButtonMain } from "../../ui/button/button";
 import CardList from "../../ui/card/cardList/cardList";
-import {
-  IoAdd,
-  IoDownloadOutline,
-  IoRemoveCircleSharp,
-} from "react-icons/io5";
+import { Layout } from "../layout/layout";
+import styles from "./customerList.module.scss";
 
 export interface CustomerListProps {}
 
 const CustomerList: React.FC<CustomerListProps> = (props) => {
   const [idCus, setIdCus] = useState(`ALL`);
   const [data, setData] = useState([]);
+  const [role, setRole] = useState("");
+  const { user } = useAppSelector((state) => state.auth);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get(
-          `${process.env.REACT_APP_BACKEND_URL}/api/customer?id=${idCus}`
-        );
+        const res = await customerService.getCustomerBoard(idCus);
         const resData = res.data.customers;
 
         setData(resData);
-      } catch (error) {
+        setRole(user.typeRole);
+      } catch (error: any) {
         console.log(error);
       }
     };
     fetchData();
-  }, [idCus]);
+  }, [idCus, user.typeRole]);
 
   return (
     <Layout>
       <div className={styles["root"]}>
-        <h1>DANH SÁCH THÀNH VIÊN</h1>
+        <h1>DANH SÁCH KHÁCH HÀNG</h1>
         <div className={styles["btn-container"]}>
           <ButtonMain>
             <IoAdd className={styles["icon"]} />
           </ButtonMain>
-          <ButtonMain>
-            <IoDownloadOutline className={styles["icon"]} />
-          </ButtonMain>
+          {role === "ADMIN" && (
+            <ButtonMain>
+              <IoDownloadOutline className={styles["icon"]} />
+            </ButtonMain>
+          )}
         </div>
         {data.length > 0 && (
           <ul className={styles["card-container"]}>
@@ -50,18 +50,18 @@ const CustomerList: React.FC<CustomerListProps> = (props) => {
               data.map((listItems: any) => {
                 return (
                   <CardList>
-                    <a href="/#">
-                      <ul>
-                        <li>{listItems.fullNameCus}</li>
-                        <li>{listItems.genderCus === 1 ? "Male" : "Female"}</li>
-                        <li>{listItems.phoneCus}</li>
-                        <li>{listItems.emailCus}</li>
-                        <li>{listItems.adressCus}</li>
+                    <ul>
+                      <li>{listItems.fullNameCus}</li>
+                      <li>{listItems.genderCus === 1 ? "Male" : "Female"}</li>
+                      <li>{listItems.phoneCus}</li>
+                      <li>{listItems.emailCus}</li>
+                      <li>{listItems.adressCus}</li>
+                      {role === "ADMIN" && (
                         <li>
                           <IoRemoveCircleSharp />
                         </li>
-                      </ul>
-                    </a>
+                      )}
+                    </ul>
                   </CardList>
                 );
               })
