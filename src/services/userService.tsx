43e15341153d3854cdbadfc7d99982b91aa79bd8
Alexander <alexanderngia@gmail.com>
2022-06-03 01:17:50 +0700

    update handleUpdateAPI

--- src/services/userService.tsx
+++ src/services/userService.tsx
@@ -5,12 +5,42 @@ const API_URL = `${process.env.REACT_APP_BACKEND_URL}/api/user/`;
 //   return axios.get(API_URL + "all");
 // };
 // const requestConfig: AxiosRequestConfig = { headers: authHeader() };
+
+export interface UpdateProps {
+  id?: number;
+  userName: string;
+  userEmail: string;
+  userPass: string;
+  userPhone: string;
+  userGender: string;
+  userAdress: string;
+  userRole: string;
+}
+
 const getUserBoard = (idUser: string | number) => {
   return axios.get(API_URL + `?id=${idUser}`);
 };
 
-const handleUpdateApi = async (updatedData: any) => {
-  return await axios.put(API_URL + "edit", updatedData);
+const handleUpdateApi = async ({
+  id,
+  userName,
+  userEmail,
+  userPass,
+  userPhone,
+  userGender,
+  userAdress,
+  userRole,
+}: UpdateProps) => {
+  return await axios.put(API_URL + "edit", {
+    id: id,
+    fullNameUser: userName,
+    emailUser: userEmail,
+    passwordUser: userPass,
+    phoneUser: userPhone,
+    genderUser: userGender,
+    adressUser: userAdress,
+    typeRole: userRole,
+  });
 };
 const handleDeleteApi = async (userId: any) => {
   return await axios.delete(API_URL + "delete", {
